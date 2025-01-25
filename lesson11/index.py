cbb78f7653b987f3b94674db2524ab28c656f783
elvedinblakqori <148322084+elvedinblakqori@users.noreply.github.com>
2025-01-25 03:41:51 -0800

    ...

--- lesson11/index.py
+++ lesson11/index.py
@@ -1,1 +1,36 @@
-def remote:
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("Year:" ,current_time.year)
+print("Month:" ,current_time.month)
+print("Day:" ,current_time.day)
+print("Hour:" ,current_time.hour)
+print("Minute:" ,current_time.minute)
+print("Second:" ,current_time.second)
+print("Microsecond:" ,current_time.microsecond)
+
+current1_time = datetime.datetime.now().date()
+
+print(current1_time)
+
+settime = datetime.datetime.now()
+
+specific_time = datetime.datetime(2020, 10, 22)
+
+
+print(specific_time.year)
+print(specific_time.month)
+print(specific_time.day)
+
+future_time = current_time + datetime.timedelta(days= 100)
+
+
+
+print(future_time)
+
+
+teksti1 = "Hello world this is the world where we drink ashav"
+
+with open("example.txt", "w") as file:
+    file.write(teksti1)
