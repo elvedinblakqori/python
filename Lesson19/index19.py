1096f7872964a25eb80fccd8c971336998288a6e
Student <elvedinblakqori2022@gmail.com>
2025-04-05 03:41:02 -0700

    ...

--- Lesson19/index19.py
+++ Lesson19/index19.py
@@ -33,13 +33,36 @@ with st.sidebar.form("book_form"):
 
 if submit_button:
     new_data = {
-        'Name': new_name
-        'Author': new_author
+        'Name': new_name,
+        'Author': new_author,
         'User Rating': new_user_rating,
-        'Price':new_review
-        'Year':new_year
+        'Price':new_review,
+        'Year':new_year,
         'Genre':new_Genre
     }
+    books_df = pd.concat([pd.DataFrame(new_data, index=[0]), books_df], ignore_index=True)
+    books_df.to_csv('bestsellers_with_categories_2023_03_27.csv', index=False)
+    st.sidebar.success("New Book Added Successfully!")
+
+st.sidebar.header("Filter Options")
+selected_author = st.sidebar.selectbox("Select Author", ['ALL'] + list(books_df['Author'].unique()))
+selected_Year = st.sidebar.selectbox("Select Year", ['ALL'] + list(books_df ['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre", ['ALL'] + list(books_df ['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum User Rating", 0.0, 5.0, 0.0, 0.1)
+max_price = st.sidebar.slider("Maximum Price", 0, books_df['Price'].max(), books_df['Price'].max())
+
+filtered_Book_df = books_df.copy()
+
+if selected_author != "ALL":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Author'] == selected_author]
+if selected_author != "ALL":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Year'] == selected_year]
+if selected_author != "ALL":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Genre'] == selected_genre]
+
+filtered_Book_df =  filtered_Book_df[(filtered_Book_df['User Rating'] >= min_rating) & filtered_Book_df['Price'] <= max_price]
+
+
 
 st.subheader("Summary")
 total_books = books_df.shape[0]
